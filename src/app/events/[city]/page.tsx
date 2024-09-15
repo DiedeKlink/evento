4a3089af21ae6259a70dd82fbd0360c648d36c19
DiedeKlink <diede@protonmail.com>
2024-09-15 16:20:22 +0200

    EventoEvent Type

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,4 +1,5 @@
 import H1 from "@/components/h1";
+import { EventoEvent } from "@/lib/types";
 
 type EventsPageProps = {
   params: {
@@ -11,7 +12,7 @@ export default async function EventsPage({ params }: EventsPageProps) {
   const response = await fetch(
     `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
   );
-  const events = await response.json();
+  const events: EventoEvent[] = await response.json();
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
---
+++ src/lib/types.ts
@@ -0,0 +1,11 @@
+export type EventoEvent = {
+  id: number;
+  name: string;
+  slug: string;
+  city: string;
+  location: string;
+  date: Date;
+  organizer: string;
+  imgUrl: string;
+  description: string;
+};
