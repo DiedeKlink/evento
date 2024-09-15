c783e9ebad9750421a4c45743bb69a57d44c34bf
DiedeKlink <diede@protonmail.com>
2024-09-15 20:17:11 +0200

    Image Component advanced

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,3 +1,6 @@
+import { EventoEvent } from "@/lib/types";
+import Image from "next/image";
+
 type EventPageProps = {
   params: {
     slug: string;
@@ -9,8 +12,33 @@ export default async function EventPage({ params }: EventPageProps) {
   const response = await fetch(
     `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
   );
-  const event = await response.json();
+  const event: EventoEvent = await response.json();
 
   console.log(event);
-  return <main>Event page</main>;
+  return (
+    <main>
+      <section className="relative h-[361px] overflow-hidden">
+        <Image
+          className="object-cover z-0 blur-3xl"
+          src={event.imageUrl}
+          alt="Event background image"
+          fill
+          quality={50}
+          sizes="(max-width: 1280px) 100vw,1280px"
+          priority
+        />
+
+        <div className="z-1 relative">
+          <Image
+            src={event.imageUrl}
+            alt={event.name}
+            width={300}
+            height={201}
+          />
+        </div>
+      </section>
+
+      <div></div>
+    </main>
+  );
 }
