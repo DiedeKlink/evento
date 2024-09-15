371fc38cca4684a85c776dbe058932c25028de92
DiedeKlink <diede@protonmail.com>
2024-09-15 19:32:52 +0200

    Event Date with Copilot

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -8,7 +8,7 @@ type EventCardProps = {
 export default function EventCard({ event }: EventCardProps) {
   return (
     <section
-      className="flex flex-col flex-1 basis-80 h-[380px] max-w-[500px] bg-white/[3%] rounded-xl overflow-hidden"
+      className="flex flex-col flex-1 basis-80 h-[380px] max-w-[500px] bg-white/[3%] rounded-xl overflow-hidden relative"
       key={event.id}
     >
       <Image
@@ -23,6 +23,17 @@ export default function EventCard({ event }: EventCardProps) {
         <p className="italic text-white/75">By {event.organizerName}</p>
         <p className="text-sm text-white/50 mt-4">{event.location}</p>
       </div>
+
+      <section className="absolute flex justify-center items-center flex-col left-[12px] top-[12px] h-[45px] w-[45px] bg-black/30 rounded-md">
+        <p className="text-xl font-bold -mb-[5px]">
+          {new Date(event.date).toLocaleDateString("en-US", {
+            day: "2-digit",
+          })}
+        </p>
+        <p className="text-xs uppercase text-accent">
+          {new Date(event.date).toLocaleString("en-US", { month: "short" })}
+        </p>
+      </section>
     </section>
   );
 }
