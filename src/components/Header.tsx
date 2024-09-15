50a9907370b127237bc367fe15a51b0466925bc9
DiedeKlink <diede@protonmail.com>
2024-09-15 10:08:51 +0200

    Logo (NextJS Image Component)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,20 @@
 import Link from "next/link";
+import Logo from "./logo";
 
 export default function Header() {
   return (
-    <header>
+    <header className="flex justify-between items-center">
       <Logo />
-      <Link href="/">Home</Link>
-      <Link href="/events/all">All Events</Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/events/all">All Events</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
