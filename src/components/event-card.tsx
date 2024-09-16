9116c15916089e9072b0a8b6f736839549f9a56a
DiedeKlink <diede@protonmail.com>
2024-09-16 08:45:50 +0200

    Fix Image

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -21,14 +21,13 @@ export default function EventCard({ event }: EventCardProps) {
           alt={event.name}
           width={500}
           height={280}
-          className="h-[60%] object-fit"
+          className="h-[60%] object-cover"
         />
         <div className="flex flex-col flex-1 justify-center items-center">
           <h2 className="text-2xl font-semibold">{event.name}</h2>
           <p className="italic text-white/75">By {event.organizerName}</p>
           <p className="text-sm text-white/50 mt-4">{event.location}</p>
         </div>
-
         <section className="absolute flex justify-center items-center flex-col left-[12px] top-[12px] h-[45px] w-[45px] bg-black/30 rounded-md">
           <p className="text-xl font-bold -mb-[5px]">
             {new Date(event.date).toLocaleDateString("en-US", {
