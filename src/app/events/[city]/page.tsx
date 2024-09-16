d3b364c4e3c7a4d0f14bf07e4ad3ee703ec641e5
DiedeKlink <diede@protonmail.com>
2024-09-16 19:17:16 +0200

    Zod validation

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -5,6 +5,7 @@ import Loading from "./loading";
 import { Metadata } from "next";
 import { capitalize } from "@/lib/utils";
 import { useSearchParams } from "next/navigation";
+import { z } from "zod";
 
 type Props = {
   params: {
@@ -29,12 +30,18 @@ export function generateMetadata({ params }: Props): Metadata {
   };
 }
 
+const pageNumberSchema = z.coerce.number().int().positive().optional();
+
 export default async function EventsPage({
   params,
   searchParams,
 }: EventsPageProps) {
   const city = params.city;
-  const page = searchParams.page || 1;
+  const parsedPage = pageNumberSchema.safeParse(searchParams.page);
+
+  if (!parsedPage.success) {
+    throw new Error("Invalid page number");
+  }
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
@@ -44,8 +51,8 @@ export default async function EventsPage({
           : `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
       </H1>
 
-      <Suspense key={city + page} fallback={<Loading />}>
-        <EventsList city={city} page={+page} />
+      <Suspense key={city + parsedPage.data} fallback={<Loading />}>
+        <EventsList city={city} page={parsedPage.data} />
       </Suspense>
     </main>
   );
